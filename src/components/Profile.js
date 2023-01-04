523e011e19b1f2de0b9ae5d9496751bf2cd6e69f
Anthony <anthonyneomh@gmail.com>
2023-01-04 18:04:59 +0700

    Updated profile picture.

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function Profile() {
     return (
         <div className="profile--container">
-            <img className="profile" src={require("../images/profilepic.jpeg")} />
+            <img className="profile" src={require("../images/namecard-photo.jpg")} />
         </div>
     )
 }
