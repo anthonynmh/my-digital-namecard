e30556af5bcd83348a93cbd052a9b291794e091a
anthonynmh <anthonyneomh@gmail.com>
2023-01-24 03:22:32 +0800

    Update path to profile photo

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -3,7 +3,8 @@ import React from "react";
 export default function Profile() {
     return (
         <div className="profile--container">
-            <img className="profile" src={require("../images/namecard-photo.jpg")} />
+            <img className="profile" src={require("../images/namecard-photo2.jpeg")} 
+                alt="profile photo" />
         </div>
     )
 }
