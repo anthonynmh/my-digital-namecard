6ff5edb907bca9b522776e73a2a088d26414b7e8
anthonynmh <anthonyneomh@gmail.com>
2023-08-01 02:08:22 +0700

    Add section for project highlights

--- src/components/Information.js
+++ src/components/Information.js
@@ -5,55 +5,60 @@ export default function Information() {
         <main className="mainContent">
             <div className="info--main">
                 <p className="name">Anthony Neo Ming Hong</p>
-                <p className="role">Aspiring Developer</p>
+                <p className="role">Aspiring Full-Stack Developer</p>
                 <p className="email">anthonyneomh@gmail.com</p>
             </div>
             <div className="info--extra">
                 <div className="about">
-                    {/*
-                    <p className="about--thoughts">
-                        Coding is art.{"\n"}
-                        Art on canvas, a painter's touch.{"\n"}
-                        Touch of perfection, lines apart.{"\n"}
-                        Apart from details, lines in part.{"\n"}
-                        Part of coding, coding of art.{"\n"}
-                    </p>
-                    */}
                     <h3>About:</h3>
                     <p>
                         Hi there!{"\n"}
-                        I'm probably coding right now to expand the list below ;){"\n"}
+                        You may click/tap my profile image for my latest resume.{"\n\n"}
+                        Documenting my journey to full-stack.{"\n"}
                     </p>
                 </div>
-                <div className="skills-container">
-                    <h3>Skills:</h3>
+                <div className="projects">
+                    <h3>Project Highlights</h3>
+                    <h4>Web Dev (HTML, CSS, JavaScript, React)</h4>
+                    <ul> 
+                        <li>
+                            <a href="https://monster-hunter-rpg-2.netlify.app/" 
+                            target="_blank"
+                            rel="noopener noreferrer">Monster Hunter RPG</a>
+                            <p>Try the game here by clicking the link!</p>
+                        </li>
+                        <li>
+                            <a href="https://anthonynmh.netlify.app/"
+                            target="_blank"
+                            rel="noopener noreferrer">My digital name card</a>
+                            <p>It's the page you're currently seeing!</p>
+                        </li>
+                    </ul>
+                    <h4>App Dev (Java, JavaFX)</h4>
                     <ul>
-                        <li>Confident language: 
-                            <span className="skills">Java</span>
-                        </li>
-                        <li>Other languages: 
-                            <span className="skills">C, Python</span>
-                        </li>
-                        <li>Backend: 
-                            <span className="skills">SQL, PostgreSQL</span>
-                        </li>
-                        <li>Frontend: 
-                            <span className="skills">HTML, CSS, JS, React</span>
-                        </li>
-                        <li>Others: 
-                            <span className="skills">Electrical and Electronics</span>
-                        </li>
-                        <li>Also: 
-                            <span className="skills">Communication, Leadership</span>
+                        <li>
+                            <a href="https://github.com/anthonynmh/tp"
+                            target="_blank"
+                            rel="noopener noreferrer">EZ-Schedule</a>
+                            <p>Download the latest release from GitHub.</p>
                         </li>
                     </ul>
                 </div>
-                {/*
-                <div className="email">
-                    <img src={require("../images/Email-icon.png")} />
-                    <p>Email</p>
+                <div className="skills-container">
+                    <h3>Summary:</h3>
+                    <div className="checks">
+                        <span>&#10003;</span>
+                        <label> Web Dev (HTML, CSS, JavaScript, React)</label><br/>
+                        <span>&#10007;</span>
+                        <label> Back End</label><br/>
+                        <span>&#10007;</span>
+                        <label> App Development (Android)</label><br/>
+                        <span>&#10007;</span>
+                        <label> App Development (IOS: Swift)</label><br/>
+                        <span>&#10003;</span>
+                        <label> App Development (Java)</label><br/>
+                    </div>
                 </div>
-                */}
             </div>
         </main>
     )
--- src/components/Profile.js
+++ src/components/Profile.js
@@ -3,8 +3,11 @@ import React from "react";
 export default function Profile() {
     return (
         <div className="profile--container">
+            <a href="https://drive.google.com/file/d/11XIIFXNYGn72vGKBXy9WDBHlNcjDTN5O/view?usp=sharing"
+            target="_blank"
+            rel="noopener noreferrer">
             <img className="profile" src={require("../images/namecard-photo2.jpeg")} 
-                alt="profile photo" />
+                alt="profile" /></a>
         </div>
     )
 }
